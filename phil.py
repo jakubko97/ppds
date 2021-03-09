5d1bf33306ae2fbfc0749600fabc8f2b0ecb4ca5
Jakub Jančík <jja@digitalsystems.eu>
2021-03-09 13:26:55 +0100

    feat(fork1,fork2): init - footman cleaned

--- phil.py
+++ phil.py
@@ -4,12 +4,13 @@ from fei.ppds import Semaphore, print, Thread
 
 PHIL_NUM = 5
 
-def phil(shared, footman, phil_id):
+def phil(forks, phil_id, fork1, fork2):
+    sleep(randint(40,50)/1000)
     while True:
         think(phil_id)
-        getForks(shared, footman, phil_id)
+        getForks(forks, phil_id, fork1, fork2)
         eat(phil_id)
-        putForks(shared, footman, phil_id)
+        putForks(forks, phil_id, fork1, fork2)
 
 def think(phil_id):
     print(f'{phil_id:02d}: thinking')
@@ -19,25 +20,37 @@ def eat(phil_id):
     print(f'{phil_id:02d}: eating')
     sleep(randint(40,50)/1000)
 
-def getForks(forks, footman, phil_id):
-    footman.wait()
+def getForks(forks, phil_id, fork1, fork2):
     print(f'{phil_id:02d}: try to get forks')
-    forks[phil_id].wait()
-    forks[(phil_id +1) % PHIL_NUM].wait()
+    forks[fork1].wait()
+    sleep(randint(40,50)/1000)
+    forks[fork2].wait()
     print(f'{phil_id:02d}: taken forks')
 
-def putForks(forks, footman, phil_id):
+def putForks(forks, phil_id, fork1, fork2):
     print(f'{phil_id:02d}: try to put forks')
-    forks[phil_id].signal()
-    forks[(phil_id +1) % PHIL_NUM].signal()
+    forks[fork1].signal()
+    forks[fork2].signal()
     print(f'{phil_id:02d}: put forks')
-    footman.signal()
 
 def main():
     forks = [Semaphore(1) for _ in range(PHIL_NUM)]
-    footman = Semaphore(PHIL_NUM-1)
 
-    phils = [Thread(phil, forks,footman, p_id) for p_id in range(PHIL_NUM)]
+    # phils = [Thread(phil, forks, p_id, p_id, (p_id+1) % PHIL_NUM) for p_id in range(PHIL_NUM)]
+
+    phils = []
+    for phil_id in range(PHIL_NUM):
+        is_leftie = randint(0,1)
+        lefties = 0
+        if is_leftie:
+            lefties += 1
+            if lefties == PHIL_NUM:
+                is_leftie = 0
+
+        if is_leftie:
+            phils.append(Thread(phil, forks, phil_id, (phil_id+1) % PHIL_NUM, phil_id))
+        else:
+            phils.append(Thread(phil, forks, phil_id, phil_id, (phil_id+1) % PHIL_NUM))
 
     for p in phils:
         p.join()
