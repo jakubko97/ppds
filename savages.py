d02002fa15171afa15b6ee2bcfc12b89ce6e70e5
Jakub Jančík <jja@digitalsystems.eu>
2021-03-23 00:17:33 +0100

    feat(cooks): add cooks list - deadlock at first cook, unfinished task

--- savages.py
+++ savages.py
@@ -8,15 +8,15 @@ from time import sleep
 Preto ich nedavame do zdielaneho objektu.
     M - pocet porcii misionara, ktore sa zmestia do hrnca.
     N - pocet divochov v kmeni (kuchara nepocitame).
+    C - pocet kucharov
+    P - pocet porcii, ktory spravi kuchar na striedacku s druhym
 """
-M = 2
+M = 3
 N = 3
-
+C = 2
+P = 1
 
 class SimpleBarrier:
-    """Vlastna implementacia bariery
-    kvoli specialnym vypisom vo funkcii wait().
-    """
 
     def __init__(self, N):
         self.N = N
@@ -26,30 +26,23 @@ class SimpleBarrier:
 
     def wait(self,
              print_str,
-             savage_id,
+             human_id,
              print_last_thread=False,
              print_each_thread=False):
         self.mutex.lock()
         self.cnt += 1
         if print_each_thread:
-            print(print_str % (savage_id, self.cnt))
+            print(print_str % (human_id, self.cnt))
         if self.cnt == self.N:
             self.cnt = 0
             if print_last_thread:
-                print(print_str % (savage_id))
+                print(print_str % (human_id))
             self.sem.signal(self.N)
         self.mutex.unlock()
         self.sem.wait()
 
 
 class Shared:
-    """V tomto pripade musime pouzit zdielanu strukturu.
-    Kedze Python struktury nema, pouzijeme triedu bez vlastnych metod.
-    Preco musime pouzit strukturu? Lebo chceme zdielat hodnotu
-    pocitadla servings, a to jednoduchsie v Pythone asi neurobime.
-    Okrem toho je rozumne mat vsetky synchronizacne objekty spolu.
-    Pri zmene nemusime upravovat API kazdej funkcie zvlast.
-    """
 
     def __init__(self):
         self.mutex = Mutex()
@@ -58,6 +51,8 @@ class Shared:
         self.empty_pot = Semaphore(0)
         self.barrier1 = SimpleBarrier(N)
         self.barrier2 = SimpleBarrier(N)
+        self.barrier3 = SimpleBarrier(C)
+        self.barrier4 = SimpleBarrier(C)
 
 
 def get_serving_from_pot(savage_id, shared):
@@ -77,11 +72,6 @@ def eat(savage_id):
 
 def savage(savage_id, shared):
     while True:
-        """Pred kazdou hostinou sa divosi musia pockat.
-        Kedze mame kod vlakna (divocha) v cykle, musime pouzit dve
-        jednoduche bariery za sebou alebo jednu zlozenu, ale kvoli
-        prehladnosti vypisov sme sa rozhodli pre toto riesenie.
-        """
 
         shared.barrier1.wait(
             "divoch %2d: prisiel som na veceru, uz nas je %2d",
@@ -105,42 +95,45 @@ def savage(savage_id, shared):
         eat(savage_id)
 
 
-def put_servings_in_pot(M, shared):
-    """M je pocet porcii, ktore vklada kuchar do hrnca.
-    Hrniec je reprezentovany zdielanou premennou servings.
-    Ta udrziava informaciu o tom, kolko porcii je v hrnci k dispozicii.
-    """
+def put_servings_in_pot(cook_id, shared):
 
-    print("kuchar: varim")
+    print("kuchar %2d: varim" % cook_id)
     # navarenie jedla tiez cosi trva...
     sleep(0.4 + randint(0, 2) / 10)
-    shared.servings += M
+    shared.servings += P
 
 
-def cook(M, shared):
-    """Na strane kuchara netreba robit ziadne modifikacie kodu.
-    Riesenie je standardne podla prednasky.
-    Navyse je iba argument M, ktorym explicitne hovorime, kolko porcii
-    ktory kuchar vari.
-    Kedze v nasom modeli mame iba jedneho kuchara, ten navari vsetky
-    potrebne porcie a vlozi ich do hrnca.
-    """
+def cook(cook_id, M, shared):
 
     while True:
+
         shared.empty_pot.wait()
-        put_servings_in_pot(M, shared)
-        shared.full_pot.signal()
+        shared.barrier3.wait(
+            "kuchar %2d: prisiel som uvarit, uz nas je %2d",
+            cook_id,
+            print_each_thread=True)
+        shared.barrier4.wait("kuchar %2d: uz sme vsetci, zaciname varit",
+                             cook_id,
+                             print_last_thread=True)
+        shared.mutex.lock()
+        if shared.servings == M:
+            print("kuchar %2d: je navarene, budim divocha" % cook_id)
+            shared.full_pot.signal()
+        shared.mutex.unlock()
+        put_servings_in_pot(cook_id, shared)
 
 
 def init_and_run(N, M):
     """Spustenie modelu"""
-    threads = list()
+    savages = list()
+    cooks = list()
     shared = Shared()
     for savage_id in range(0, N):
-        threads.append(Thread(savage, savage_id, shared))
-    threads.append(Thread(cook, M, shared))
+        savages.append(Thread(savage, savage_id, shared))
+    for cook_id in range(0, C):
+        cooks.append(Thread(cook,cook_id, M, shared))
 
-    for t in threads:
+    for t in savages + cooks:
         t.join()
 
 
