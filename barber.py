acecc65aaa79616aba3da4d077162a0408e38e6c
Jakub Jančík <jja@digitalsystems.eu>
2021-03-29 19:17:59 +0200

    feat(queue): fifo queue - formatting and refactoring code - add barber semaphore per thread - add lock and unlock with shared access

--- barber.py
+++ barber.py
@@ -11,33 +11,28 @@ signalizuje „customerDone‟ a čaká na „barberDone‟
 
 from random import randint
 from time import sleep
-from fei.ppds import print, Thread, Mutex, Semaphore
+from fei.ppds import print, Thread, Mutex, Semaphore, Event
 
-N = 5
-
-class LightSwitch(object):
-    def __int__(self):
-        self.cnt = 0
+class Shared(object):
+    def __init__(self, N):
+        self.N = N
+        self.barberDone = Semaphore(0)
+        self.customerDone = Semaphore(0)
         self.mutex = Mutex()
+        self.customers = 0
+        self.customer = Semaphore(0)
 
-    def wait(self, sem):
-        self.mutex.lock()
-        self.cnt += 1
-        if self.cnt == 1:
-            sem.wait()
-        self.mutex.unlock()
+        self.queue = []
 
-    def signal(self, sem):
-        self.mutex.lock()
-        self.cnt -= 1
-        if self.cnt == 0:
-            sem.signal()
-        self.mutex.unlock()
-
-def barber(barber_id ,shared):
+def barber(barber_id, shared):
     while True:
+
         shared.customer.wait()
-        shared.barber.signal()
+        shared.mutex.lock()
+        barber = shared.queue.pop(0)
+        shared.mutex.unlock()
+
+        barber.signal()
 
         cutHair(barber_id)
 
@@ -45,43 +40,58 @@ def barber(barber_id ,shared):
         shared.barberDone.signal()
 
 def balk(customer_id):
-    print(f"Customer {customer_id} : leaving")
-    sleep(randint(0,2)/10)
+    print(f"Customer {customer_id} : balk.")
+    sleep(.1)
 
 def getHairCut(customer_id):
-    print(f"Customer {customer_id} : getting haircut")
-    sleep(2/10 + 0.4)
+    print(f"Customer {customer_id} : getting haircut.")
+    sleep(randint(2,4)/100)
 
 def cutHair(barber_id):
-    print(f"Barber {barber_id} : cutting hair")
-    sleep(2/10 + 0.4)
+    print(f"Barber {barber_id}: cutting hair.")
+    sleep(randint(2,4)/100)
+
+def customerExit(customer_id):
+    print(f"Customer {customer_id}: exited.")
+    sleep(randint(1,2)/100)
+
+def customerEnter(customer_id):
+    print(f"Customer {customer_id}: entered.")
+    sleep(randint(9,10)/100)
 
 def customer(customer_id, shared):
+    barber = Semaphore(0)
+
     while True:
-        if shared.customers == N:
+        shared.mutex.lock()
+        customerEnter(customer_id)
+        if shared.customers == shared.N:
             balk(customer_id)
-        shared.customers +=1
+            shared.mutex.unlock()
+        else:
+            shared.customers +=1
+            shared.queue.append(barber)
+            shared.mutex.unlock()
 
-        shared.customer.signal()
-        shared.barber.wait()
+            shared.customer.signal()
+            barber.wait()
 
-        shared.customerDone.signal()
-        shared.barberDone.wait()
+            getHairCut(customer_id)
 
-        shared.customers -= 1
+            shared.customerDone.signal()
+            shared.barberDone.wait()
 
-class Shared(object):
-    def __init__(self):
-        self.barberDone = Semaphore(0)
-        self.customerDone = Semaphore(0)
-        self.barber = Semaphore(0)
-        self.mutex = Mutex()
-        self.customers = 0
-        self.customer = Semaphore(0)
+            shared.mutex.lock()
+            shared.customers -= 1
+            customerExit(customer_id)
+            shared.mutex.unlock()
+
+def run_model():
+
+    s = Shared(5)
+    b = [Thread(barber, i, s) for i in range(2)]
+    c = [Thread(customer, i, s) for i in range(10)]
 
 
-s = Shared()
-barber = [Thread(barber, i ,s) for i in range(1)]
-customer = [Thread(customer, i, s) for i in range(N)]
-for p in barber + customer:
-    p.join()
+if __name__ == "__main__":
+    run_model()
