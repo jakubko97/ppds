6f1ee64172fa28c20cd10303ed0e52cf72b71c81
Jakub Jančík <jja@digitalsystems.eu>
2021-04-13 11:47:30 +0200

    feat urllib.request - change asyncio for urllib.request

--- hviezda.py
+++ hviezda.py
@@ -1,26 +1,34 @@
 import queue
 import time
-import asyncio
+import urllib.request
 
-async def task(name, work_queue):
+def task(name, work_queue):
     if work_queue.empty():
         print(f'Task {name} nothing to do')
         return
 
     while not work_queue.empty():
-        delay = await work_queue.get()
+        url = work_queue.get()
 
-        print(f'Task {name} running')
+        print(f'Task {name} getting url: {url}')
         time_start = time.perf_counter()
-        await asyncio.sleep(delay)
+        urllib.request.urlopen(url)
         elapsed = time.perf_counter() - time_start
         print(f'Task {name} elapsed time: {elapsed: .1f}')
+        yield
 
-async def main():
-    work_queue = asyncio.Queue()
+def main():
+    work_queue = queue.Queue()
 
-    for work in [3,1,9,17]:
-        await work_queue.put(work)
+    for url in {
+        'http://google.com',
+        'http://microsoft.com',
+        'http://facebook.com',
+        'http://twitter.com',
+        'http://stuba.sk',
+        'http://uim.fei.stuba.sk',
+    }:
+        work_queue.put(url)
 
     tasks = [
         task( 'One', work_queue),
@@ -28,10 +36,19 @@ async def main():
     ]
 
     done = False
+
     time_start = time.perf_counter()
-    await asyncio.gather(*tasks)
+    while not done:
+        for t in tasks:
+            try:
+                next(t)
+            except StopIteration:
+                tasks.remove(t)
+            if not tasks:
+                done = True
+
     elapsed = time.perf_counter() - time_start
     print(f'Total  elapsed time: {elapsed: .1f}')
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
